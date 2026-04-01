8800ce8dde80fabea2333127a880d704e615cc34
kn1ghtOwl <vinayak03.2003@gmail.com>
2026-04-01 20:08:47 +0530

    setting up users routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 5001
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes)
 
 connectDb().then(() => {
     app.listen(PORT, () => {
