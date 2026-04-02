44617ed9692a2fbf0900833f1b7e1725aff5615c
kn1ghtOwl <vinayak03.2003@gmail.com>
2026-04-02 19:19:14 +0530

    added like and unlike feature

--- backend/src/controllers/postController.js
+++ backend/src/controllers/postController.js
@@ -44,4 +44,30 @@ const getPost = async (req, res) => {
     }
 }
 
-export {createPost, getPost};
+const likeUnlikePost = async (req, res) => {
+    try {
+        const {id: postId} = req.params;
+        const userId = req.user.username;
+
+        const post = await Post.findById(postId);
+        if(!post) {
+            return res.status(404).json({message: "Post not found!"})
+        }
+
+        const userLikedPost = post.likes.includes(userId);
+        if(userLikedPost){
+            await Post.updateOne({_id: postId}, {$pull: {likes: userId}});
+            res.status(200).json({message: "Post unliked!"})
+        } else{
+            post.likes.push(userId);
+            await post.save()
+            res.status(500).json({message: "Post liked!"})
+        }
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log("Error in likeUnlikePost: ", error.message);
+    }
+}
+
+export {createPost, getPost, likeUnlikePost};
--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import {createPost, getPost} from "../controllers/postController.js"
+import {createPost, getPost, likeUnlikePost} from "../controllers/postController.js"
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getPost);
 router.post("/create", authMiddleware, createPost);
+router.post("/like/:id", authMiddleware, likeUnlikePost);
 
 export default router;
