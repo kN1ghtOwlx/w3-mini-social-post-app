1e90b9fdf9ca479ee31e522f65cc314f7594a549
kn1ghtOwl <vinayak03.2003@gmail.com>
2026-04-02 15:15:45 +0530

    added logout features

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -66,4 +66,14 @@ const loginUser = async (req, res) => {
     }
 }
 
-export {signupUser, loginUser};
+const logoutUser = (req, res) => {
+    try {
+        res.cookie("jwt", "", {maxAge: 1});
+        res.status(200).json({message: "User logged out successfully"})
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        console.log("Error in logoutUSer: ", error.message)
+    }
+}
+
+export {signupUser, loginUser, logoutUser};
