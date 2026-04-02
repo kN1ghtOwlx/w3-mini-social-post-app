1e67a8faae280e6cf951b5e19bc362bb7ab0f5ca
kn1ghtOwl <vinayak03.2003@gmail.com>
2026-04-02 19:38:27 +0530

    added comments features

--- backend/src/routes/postRoutes.js
+++ backend/src/routes/postRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createPost, getPost, likeUnlikePost} from "../controllers/postController.js"
+import {commentToPost, createPost, getPost, likeUnlikePost} from "../controllers/postController.js"
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/", getPost);
 router.post("/create", authMiddleware, createPost);
 router.post("/like/:id", authMiddleware, likeUnlikePost);
+router.post("/comment/:id", authMiddleware, commentToPost)
 
 export default router;
