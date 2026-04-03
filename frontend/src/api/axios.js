1a8c77a0a464523e080d0aad20a9e72d7054e292
kn1ghtOwl <vinayak03.2003@gmail.com>
2026-04-03 17:46:59 +0530

    fix axios baseURL to render

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: "https://w3-mini-social-post-app.onrender.com/",
     withCredentials: true
 })
 
